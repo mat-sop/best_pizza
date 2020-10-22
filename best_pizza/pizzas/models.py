12296f9046f4d2132b29cfcd83b8ed2d51e91653
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2020-10-22 22:14:00 +0200

    Add initial definitions of models

--- best_pizza/pizzas/models.py
+++ best_pizza/pizzas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Pizza(models.Model):
+    created = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    restaurant = models.ForeignKey(Restaurant, blank=True, on_delete=models.CASCADE)
+    toppings = models.ManyToManyField(Topping, blank=True)
+    number_of_votes = models.PositiveIntegerField(default=0)
