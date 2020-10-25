5e19d16d2dbe557a27fb0aae3340dcb26fb04169
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2020-10-25 17:42:29 +0100

    Add view for voting

--- best_pizza/pizzas/urls.py
+++ best_pizza/pizzas/urls.py
@@ -5,6 +5,7 @@ from pizzas import views
 urlpatterns = [
     path('pizzas/', views.PizzaList.as_view()),
     path('pizzas/<int:pk>/', views.PizzaDetail.as_view()),
+    path('pizzas/<int:pk>/vote/', views.pizza_vote),
     path('restaurants/', views.RestaurantList.as_view()),
     path('restaurants/<int:pk>/', views.RestaurantDetail.as_view()),
     path('toppings/', views.ToppingList.as_view())
--- best_pizza/pizzas/views.py
+++ best_pizza/pizzas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from pizzas.models import Pizza, Restaurant, Topping
 from pizzas.serializers import (PizzaSerializer, RestaurantSerializer,
@@ -29,3 +31,17 @@ class RestaurantDetail(generics.RetrieveUpdateDestroyAPIView):
 class ToppingList(generics.ListCreateAPIView):
     queryset = Topping.objects.all()
     serializer_class = ToppingSerializer
+
+
+@api_view(['GET'])
+def pizza_vote(request, pk):
+    try:
+        pizza = Pizza.objects.get(pk=pk)
+        pizza.number_of_votes += 1
+        pizza.save()
+    except Pizza.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    return Response(status=status.HTTP_202_ACCEPTED)
+
+
